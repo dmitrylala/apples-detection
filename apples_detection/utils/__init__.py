3df574b47c2e7b955d7c806f69dbe8a0453fa4ec
Dmitry Nesterov <dim.nesterov2015@gmail.com>
2023-03-18 14:47:11 +0300

    added patchifier

--- apples_detection/utils/__init__.py
+++ apples_detection/utils/__init__.py
@@ -1,8 +1,12 @@
+from .patchify import Patchifier, SmartPatchifier
 from .pylogger import get_pylogger
 from .utils import (
+    add_leading_zeros,
+    add_suffix,
     get_metric_value,
     instantiate_callbacks,
     instantiate_loggers,
     log_hyperparameters,
+    reverse_one_hot,
     task_wrapper,
 )
