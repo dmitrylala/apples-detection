08f48b550f82115a37a097a3214c6217447ea663
Dmitry Nesterov <dim.nesterov2015@gmail.com>
2023-03-18 16:11:47 +0300

    set timeout to 1 sec

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,7 +17,7 @@ from apples_detection.utils import reverse_one_hot
         (0, (15, 256, 256)),
     ],
 )
-@pytest.mark.timeout(0.5)
+@pytest.mark.timeout(1)
 def test_reverse_one_hot(num_instances: int, size: Tuple[int, int]):
     gt_mask = torch.randint(high=num_instances + 1, size=size)
     one_h = one_hot(gt_mask, num_classes=num_instances + 1).permute(0, 3, 1, 2)
